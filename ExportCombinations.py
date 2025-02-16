aae1ddc74aadd5b7d8b1085e69294c0041cfd444
MooreAx <moore.ax@gmail.com>
2025-02-16 09:43:38 -0600

    max columns
    adding max columns for printing to terminal

--- ExportCombinations.py
+++ ExportCombinations.py
@@ -22,24 +22,33 @@ def generate_day_matrix(best_combinations, month_start_date):
             for day_idx in range(start_day_idx, end_day_idx + 1):
                 matrix[day_idx][col_idx] += f"{p['Name']}"
 
+    max_solns = 10
+
     # Print the header
     print(f"{'Day':<10}", end=" | ")
-    for col_idx in range(len(best_combinations)):
+    for col_idx in range(min(len(best_combinations), max_solns)):
         print(f"Pairing {col_idx + 1:<12}", end=" | ")
     print()
 
 
     # Print the header row (Max consecutive days off)
     print(f"{' ' * 10}", end=" | ")  # Align with "Day" column
+    i=0
     for _, max_days_off, _ in best_combinations:
+        i += 1
+        if i > max_solns:
+            continue
         print(f"Max CDO: {max_days_off:<11}", end=" | ")
+
     print()
 
+    #reduce the size
+    truncated_matrix = [row[:10] for row in matrix]
 
     # Print the day matrix
     for day_idx, day in enumerate(days):
         print(f"{day.strftime('%a %d %b'):<10}", end=" | ")
-        for col in matrix[day_idx]:
+        for col in truncated_matrix[day_idx]:
             print(f"{col:<20}", end=" | ")
         print()
 
@@ -55,8 +64,6 @@ month_start_date = date(2025, 3, 1)
 print("\n\n")
 
 generate_day_matrix(best_combinations, month_start_date)
-
-
 
 
 
